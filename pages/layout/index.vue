7df3bbf004c26f644ea408f4cb4bfe0835e34c9c
黄晓燕 <huangxiaoyan@huangxiaoyandeMacBook-Pro.local>
2021-05-13 15:53:26 +0800

    first commit

---
+++ pages/layout/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!-- 顶部导航栏 -->
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <nuxt-link
+          to="/"
+          class="navbar-brand"
+        >conduit</nuxt-link>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <nuxt-link
+              to="/"
+              class="nav-link"
+              exact
+            >Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link
+              to="/editor"
+              class="nav-link">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link
+              to="/setting"
+              class="nav-link">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link
+              to="login"
+              class="nav-link">Sign up</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link
+              to="/profile/1"
+              class="nav-link"
+            >
+              <img
+                class="user-pic"
+                src="https://dss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=265704898,674087460&fm=58"
+                alt="avator" />
+                hxy716
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- 子路由 -->
+    <nuxt-child />
+
+    <!-- 底部 -->
+    <footer>
+      <div class="container">
+        <nuxt-link
+          to="/"
+          class="logo-font">conduit</nuxt-link>
+        <span class="attribution">
+          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+        </span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
