b884644137d226ff77b8ecb6e752b610551ef834
黄晓燕 <huangxiaoyan@huangxiaoyandeMacBook-Pro.local>
2021-05-18 17:58:04 +0800

    feat: 评论功能

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -26,51 +26,7 @@
         <div class="row">
 
         <div class="col-xs-12 col-md-8 offset-md-2">
-
-            <form class="card comment-form">
-            <div class="card-block">
-                <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
-            </div>
-            <div class="card-footer">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-                <button class="btn btn-sm btn-primary">
-                Post Comment
-                </button>
-            </div>
-            </form>
-            
-            <div class="card">
-            <div class="card-block">
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-                </a>
-                &nbsp;
-                <a href="" class="comment-author">Jacob Schmidt</a>
-                <span class="date-posted">Dec 29th</span>
-            </div>
-            </div>
-
-            <div class="card">
-            <div class="card-block">
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-                </a>
-                &nbsp;
-                <a href="" class="comment-author">Jacob Schmidt</a>
-                <span class="date-posted">Dec 29th</span>
-                <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-                </span>
-            </div>
-            </div>
-            
+            <comment :article="article" />
         </div>
 
         </div>
@@ -84,11 +40,13 @@
 import { getArticle } from '@/api/articles';
 import MarkdownIt from 'markdown-it';
 import ArticleMeta from './components/article-meta'
+import Comment from './components/comment'
 
 export default {
     name: 'ArticlePage',
     components: {
-        ArticleMeta
+        ArticleMeta,
+        Comment
     },
     head(){
         return {
